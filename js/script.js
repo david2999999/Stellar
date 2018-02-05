c547dd13b510e2baf9eb30a17acc1a15a630f0bb
David Jiang <djiang86@binghamton.edu>
2018-02-05 23:27:00 +0000

    linked form to task list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var utility = utilities();
 var idGenerator = utility.idGen(0)
-var idGenerator2 = utility.idGen(0)
+var idGenerator2 = utility.idGen(1)
 
 var User = Backbone.Model.extend({
     defaults: {
@@ -19,40 +19,44 @@ var User = Backbone.Model.extend({
 var Users = Backbone.Collection.extend({});
 
 var Task = Backbone.Model.extend({
-   id: 0,
-    taskTitle: "",
-   taskDesc: "",
-   dueDate: new Date()
+    defaults: {
+        id: 0,
+        taskTitle: "",
+        taskDesc: "",
+        dateCreated: utility.formatDate(new Date()),
+        dueDate: '',
+        img: ''
+    }
 });
 
 var Tasks = Backbone.Collection.extend({});
 
 var user1 = new User({
     id: idGenerator() ,
-    fullName: 'David T.',
+    fullName: 'CHRISTOPHER T.',
     role: "Front-End Developer",
     authenticate: true,
-    taskTitle: "MongoDB",
+    taskTitle: "JAVASCRIPT",
     taskNumber: 4,
-    taskDescription: "MongoDB developers who use the program regularly should have an idea of how to do specific commands. There are some of more common ones that they should be able to talk about, including collection commands.",
+    taskDescription: "Document traversal and manipulation, event handling, animation, and Ajax",
     taskAssignDate: utility.formatDate(new Date()),
     taskDueDate: "February 10 2018"
 });
 var user2 = new User({
     id: idGenerator() ,
-    fullName: "Joshua Q.",
-    role: "Back-End Developer",
-    taskTitle: "Javascript",
+    fullName: "JOHN SMITH",
+    role: "FULL STACK DEVELOPER",
+    taskTitle: "MYSQL",
     authenticate: true,
     taskNumber: 4,
-    taskDescription: "Create,Read,Update,Delete users and tasks.",
+    taskDescription: "Create complex queries and improve your database application performance. Explore data design and more.",
     taskAssignDate: utility.formatDate(new Date()),
     taskDueDate: "April 4 2018"
 });
 var user3 = new User({
     id: idGenerator() ,
-    fullName: "David T.",
-    role: "Front-End Developer",
+    fullName: "CHRISTY SU",
+    role: "BACK-END DEVELOPER",
     authenticate: true,
     taskTitle: "MongoDB",
     taskNumber: 4,
@@ -62,43 +66,97 @@ var user3 = new User({
 });
 var user4 = new User({
     id: idGenerator() ,
-    fullName: "Joshua Q.",
+    fullName: "SAMUEL MA",
     role: "Back-End Developer",
-    taskTitle: "Javascript",
+    taskTitle: "NODEJS",
     authenticate: true,
-    taskNumber: 4,
-    taskDescription: "Create,Read,Update,Delete users and tasks.",
+    taskNumber: 5,
+    taskDescription: "responsible for writing server-side web application logic in JavaScript and/or variants of it, such as CoffeeScript, IcedCoffeeScript,",
     taskAssignDate: utility.formatDate(new Date()),
     taskDueDate: "April 18 2018"
 });
-var users = new Users([user1, user2, user3, user4]);
+var user5 = new User({
+    id: idGenerator() ,
+    fullName: "LOUIS J.",
+    role: "FULL-STACK DEVELOPER",
+    authenticate: true,
+    taskTitle: "JAVA",
+    taskNumber: 3,
+    taskDescription: "Connect the website to the database, which will be used to persist data of models and users",
+    taskAssignDate: utility.formatDate(new Date()),
+    taskDueDate: "April 17 2018"
+});
+var user6 = new User({
+    id: idGenerator() ,
+    fullName: "LIA SMITH",
+    role: "FRONT-END DEVELOPER",
+    authenticate: true,
+    taskTitle: "MYSQL",
+    taskNumber: 7,
+    taskDescription: "Create complex queries and improve your database application performance. Explore data design and more.",
+    taskAssignDate: utility.formatDate(new Date()),
+    taskDueDate: "April 17 2018"
+});
+var users = new Users([user1, user2, user3, user4,user5,user6]);
 
 
 var task1 = new Task({
     id: idGenerator2(),
     taskTitle: "HTML/CSS",
     taskDesc: "Create Front page of Website, deciding on User interfaces such as color theme and layout",
-    dueDate: "Due Date: 2/21/2018"
+    dueDate: "FEBRUARY 21 2018",
+    img: 'icon-software-font-underline'
 });
 var task2 = new Task({
     id: idGenerator2(),
     taskTitle: "BACKBONEJS",
     taskDesc: "Using the models, create a view using the template and then inject the views with attributes of the models.",
-    dueDate: "Due Date: 2/30/2018"
+    dueDate: "FEBRUARY 30 2018",
+    img: 'icon-software-horizontal-align-center'
 });
 var task3 = new Task({
     id: idGenerator2(),
     taskTitle: "java",
     taskDesc: "Connect the website to the database, which will be used to persist data of models and users",
-    dueDate: "Due Date: 2/29/2018"
+    dueDate: "April 17 2018",
+    img: 'icon-software-layers2'
 });
 var task4 = new Task({
     id: idGenerator2(),
     taskTitle: "javascript",
     taskDesc: "Create,Read,Update,Delete users and tasks",
-    dueDate: "Due Date: 2/22/2018"
+    dueDate: "April 28 2018",
+    img: 'icon-software-horizontal-align-left'
 });
-var tasks = new Tasks([task1, task2, task3, task4]);
+var task5 = new Task({
+    id: idGenerator2(),
+    taskTitle: "NODEJS",
+    taskDesc: "Responsible for writing server-side web application logic in JavaScript and/or variants of it, such as CoffeeScript, IcedCoffeeScript.",
+    dueDate: "April 30 2018",
+    img: 'icon-software-indent-left'
+});
+var task6 = new Task({
+    id: idGenerator2(),
+    taskTitle: "MONGODB",
+    taskDesc: "MongoDB developers who use the program regularly should have an idea of how to do specific commands. There are some of more common ones that they should be able to talk about, including collection commands.",
+    dueDate: "February 10 2018",
+    img: 'icon-software-vertical-align-bottom'
+});
+var task7 = new Task({
+    id: idGenerator2(),
+    taskTitle: "MYSQL",
+    taskDesc: "Create complex queries and improve your database application performance. Explore data design and more.",
+    dueDate: "February 30 2018",
+    img: 'icon-software-layout-header-4boxes'
+});
+var task8 = new Task({
+    id: idGenerator2(),
+    taskTitle: "JQUERY",
+    taskDesc: "Document traversal and manipulation, event handling, animation, and Ajax",
+    dueDate: "February 30 2018",
+    img: 'icon-software-layout-header-4boxes'
+});
+var tasks = new Tasks([task1, task2, task3, task4, task5, task6, task7, task8]);
 // =================================================================
 
 var UserView = Backbone.View.extend({
@@ -128,7 +186,7 @@ var UserView = Backbone.View.extend({
             '                        <input type="text" class="form__input" value="' + this.model.get('taskTitle') +'"id="task">');
 
         $('.task-description').html(' <textarea name="task-descrip" id="task-description" cols="50" rows="8" maxlength="500">' + this.model.get('taskDescription')+ '</textarea>' +
-                                '<label for="task-description"></label>')
+                                '<label for="task-description"></label>');
         },
 
     delete: function () {
@@ -173,7 +231,22 @@ var TaskView = Backbone.View.extend({
 
     events: {
         'click .delete-task' : 'delete',
+        'click .edit-task' : 'edit'
 
+    },
+
+    edit: function () {
+        $('.task-title').html('<label for="task-title" class="form__label">Task Title</label>' +
+            '<input type="text" class="form__input" placeholder="MongoDb" value="' + this.model.get('taskTitle') +'" id="task-title">');
+
+        $('.description').html('<textarea name="task-descrip" id="description" cols="50" rows="8" maxlength="500">' + this.model.get('taskDesc')+ '</textarea>' +
+            '<label for="description"></label>');
+
+        $('.date-created').html('<label for="date-created" class="form__label">Date Created</label>' +
+            '<input type="text" class="form__input" value="' + this.model.get('dateCreated') +'" id="date-created" required>');
+
+        $('.due-date').html('<label for="due-date" class="form__label">Due Date</label>' +
+            '<input type="text" class="form__input" value="' + this.model.get('dueDate') +'" id="due-date" required>')
     },
 
     delete: function () {
@@ -182,10 +255,12 @@ var TaskView = Backbone.View.extend({
 
     initialize: function () {
         this.template = _.template($('.task-list-template').html());
+
     },
 
     render: function () {
         this.$el.html(this.template(this.model.toJSON()));
+        this.$('.task-list__item-icon').addClass(this.model.get('img'));
         return this;
     }
 });
@@ -213,7 +288,7 @@ var userViews = new UserViews();
  userViews.render();
 
 var taskViews = new TaskViews();
-// taskViews.render();
+taskViews.render();
 
 
 
