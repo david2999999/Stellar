f98ee27bb60628efe28a3e681611a5ac7e56e988
David Jiang <djiang86@binghamton.edu>
2018-04-30 20:15:17 +0000

    added forms for create and edit users and task

--- Stellar-Project/src/main/java/com/stellar/spring/controller/TaskController.java
+++ Stellar-Project/src/main/java/com/stellar/spring/controller/TaskController.java
@@ -1,5 +1,18 @@
 package com.stellar.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TaskController {
 
+	@GetMapping("/task/{id}/update")
+	public String editUserForm() {
+		return "/forms/EditTaskForm";
+	}
+	
+	@GetMapping("/task/new")
+	public String createTaskForm() {
+		return "/forms/CreateTaskForm";
+	}
 }
--- Stellar-Project/src/main/java/com/stellar/spring/controller/UserController.java
+++ Stellar-Project/src/main/java/com/stellar/spring/controller/UserController.java
@@ -1,5 +1,18 @@
 package com.stellar.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+	@GetMapping("/user/{id}/update")
+	public String editUserForm() {
+		return "/forms/EditUserForm";
+	}
+	
+	@GetMapping("/user/new")
+	public String createUserForm() {
+		return "/forms/CreateUserForm";
+	}
 }
